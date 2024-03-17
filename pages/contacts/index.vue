28823ff4aaec0db619415b9cf8ba3ad1a593880b
Alexey Baikov <baikov52@gmail.com>
2024-03-17 17:11:36 +0300

    ~ fix contacts

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -28,11 +28,6 @@ const breadcrumbs = [
       <div class="mx-auto w-full max-w-7xl p-4">
         <div class="block rounded-lg">
           <div class="flex flex-wrap items-center">
-            <div class="block w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
-              <div class="block w-full overflow-hidden rounded-lg shadow-lg">
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A832f2b93d8e203a25b2eaf25f4da38f4a271b283c8a15d665841d9eb563e04c4&amp;source=constructor&amp;scroll=false" width="100%" height="400" frameborder="0" />
-              </div>
-            </div>
             <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
               <div class="flex flex-wrap pt-12 lg:pt-0">
                 <div class="mb-12 w-full shrink-0 grow-0 basis-auto px-3 md:w-6/12 md:px-6 lg:w-full xl:w-6/12 xl:px-12">
@@ -86,6 +81,11 @@ const breadcrumbs = [
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+            <div class="block w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+              <div class="block w-full overflow-hidden rounded-lg shadow-lg">
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A832f2b93d8e203a25b2eaf25f4da38f4a271b283c8a15d665841d9eb563e04c4&amp;source=constructor&amp;scroll=false" width="100%" height="400" frameborder="0" />
               </div>
             </div>
           </div>
