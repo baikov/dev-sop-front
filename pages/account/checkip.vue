786a834a044c9c64708ab2be4de9977d50322543
Alexey Baikov <baikov52@gmail.com>
2024-05-10 13:38:25 +0300

    ~ fix check ip page

--- pages/account/checkip.vue
+++ pages/account/checkip.vue
@@ -13,7 +13,10 @@ const endpoint = computed(
   () => `${config.public.apiUrl}/forms/check-ip`
 )
 const { data: ip, refresh } = await useFetch(endpoint)
-const jsip = useState('ip')
+
+const realIP = useState<string | string[]>('realIP')
+const forwardedFor = useState<string | string[]>('forwardedFor')
+const socketAddr = useState<string>('socketAddr')
 </script>
 
 <template>
@@ -21,14 +24,22 @@ const jsip = useState('ip')
     <CommonBreadcrumbs :items="breadcrumbs" />
     <UContainer class="flex flex-col items-center justify-center">
       <div class="flex flex-col items-start justify-center gap-4">
+        <span class="text-lg font-bold">Backend:</span>
         <span>
           REMOTE_ADDR: <span class="font-bold" :class="ip.remote_addr ? 'text-green-700' : 'text-red-700'">{{ ip.remote_addr || 'Не определен' }}</span>
         </span>
         <span>
           HTTP_X_FORWARDED_FOR: <span class="font-bold" :class="ip.forwarded ? 'text-green-700' : 'text-red-700'">{{ ip.forwarded || 'Не определен' }}</span>
         </span>
+        <span class="text-lg font-bold">Frontend:</span>
         <span>
-          jsip: <span class="font-bold" :class="jsip ? 'text-green-700' : 'text-red-700'">{{ jsip || 'Не определен' }}</span>
+          x-real-ip: <span class="font-bold">{{ realIP }}</span>
+        </span>
+        <span>
+          x-forwarded-for: <span class="font-bold">{{ forwardedFor }}</span>
+        </span>
+        <span>
+          socket.remoteAddress: <span class="font-bold">{{ socketAddr }}</span>
         </span>
         <UButton @click="refresh">
           Refresh
--- plugins/ip.server.ts
+++ plugins/ip.server.ts
@@ -1,12 +1,18 @@
 export default defineNuxtPlugin(() => {
-  const ip = useState<string | string[]>('ip', () => '')
+  const realIP = useState<string | string[]>('realIP', () => '')
+  const forwardedFor = useState<string | string[]>('forwardedFor', () => [])
+  const socketAddr = useState<string>('socketAddr', () => '')
 
   const event = useRequestEvent()
   if (event) {
   // attempt to get IP
-    ip.value = event.node.req.headers['x-real-ip'] || event.node.req.headers['x-forwarded-for'] || event.node.req.socket?.remoteAddress || ''
+    realIP.value = event.node.req.headers['x-real-ip'] || 'No'
+    forwardedFor.value = event.node.req.headers['x-forwarded-for'] || 'No'
+    socketAddr.value = event.node.req.socket?.remoteAddress || 'No'
   } else {
     // fallback to window object
-    ip.value = 'No'
+    realIP.value = 'No event'
+    forwardedFor.value = 'No event'
+    socketAddr.value = 'No event'
   }
 })
