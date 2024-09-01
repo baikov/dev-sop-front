ce292e4242181a3c87e9404c3516aa32acde51dc
Alexey Baikov <baikov52@gmail.com>
2024-09-01 17:23:11 +0300

    + add confidentiality page

---
+++ pages/company/confidentiality.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Политика в области обработки и защиты персональных данных — СПЕЦОПТТОРГ',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Политика в области обработки и защиты персональных данных — СПЕЦОПТТОРГ'
+    }
+  ]
+})
+const breadcrumbs = [
+  {
+    level: 1,
+    name: 'Политика конфиденциальности',
+    href: '/confidentiality',
+    disabled: true
+  }
+]
+</script>
+
+<template>
+  <div>
+    <CommonBreadcrumbs :items="breadcrumbs" />
+    <CommonPageHeader h1="Политика конфиденциальности СПЕЦОПТТОРГ" />
+    <div class="mx-auto w-full max-w-7xl p-4">
+      <CommonConfidentiality />
+    </div>
+  </div>
+</template>
