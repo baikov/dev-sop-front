373474037f63a4f328e18577975e70bdc33c09c1
Alexey Baikov <baikov52@gmail.com>
2024-09-01 17:22:29 +0300

    + add payment page

---
+++ pages/company/payment.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Информация об оплате',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Информация об оплате стальной трубы, металлопроката и изоляции для труб'
+    }
+  ]
+})
+const breadcrumbs = [
+  {
+    level: 1,
+    name: 'Оплата',
+    href: '/payment',
+    disabled: true
+  }
+]
+</script>
+
+<template>
+  <div>
+    <CommonBreadcrumbs :items="breadcrumbs" />
+    <CommonPageHeader h1="Оплата продукции компании СПЕЦОПТТОРГ" />
+    <div class="mx-auto w-full max-w-7xl p-4">
+      <CommonPayment />
+    </div>
+  </div>
+</template>
