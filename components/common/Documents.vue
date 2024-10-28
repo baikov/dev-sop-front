1a36303153a62604cb09a08be1101b050906c103
Alexey Baikov <baikov52@gmail.com>
2024-10-28 15:37:21 +0300

    ~ fix docs

--- components/common/Documents.vue
+++ components/common/Documents.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { fileSize } from '~/utils/base'
 import type { IDocument } from '~/types/catalog'
+
 const config = useRuntimeConfig()
 
 defineProps<{
@@ -16,21 +17,33 @@ const fileIconMapping = {
   xlsx: 'i-bi-filetype-xlsx',
   png: 'i-bi-filetype-png',
   jpg: 'i-bi-filetype-jpg',
-  jpeg: 'i-bi-filetype-jpg'
+  jpeg: 'i-bi-filetype-jpg',
 }
 </script>
 
 <template>
-  <div v-if="docs" class="grid grid-cols-1 gap-4" :class="isFull ? '' : 'md:grid-cols-2 lg:grid-cols-3'">
+  <div
+    v-if="docs"
+    class="grid grid-cols-1 gap-4"
+    :class="isFull ? '' : 'md:grid-cols-2 lg:grid-cols-3'"
+  >
     <UCard
       v-for="doc in docs"
       :key="doc.id"
-      :ui="isFull ? { body: {padding: 'px-1 py-1 sm:p-2'}} : ''"
+      :ui="isFull ? { body: { padding: 'px-1 py-1 sm:p-2' } } : {}"
     >
       <div class="flex items-center justify-between">
         <div class="flex items-center">
-          <UIcon :name="fileIconMapping[doc.file.split('.').slice(-1)[0] as keyof typeof fileIconMapping]" class="mr-2 size-5 shrink-0" />
-          <NuxtLink :to="`${config.public.mediaUrl}/${doc.file}`" class="underline hover:text-gray-700 dark:hover:text-gray-200" external target="_blank">
+          <UIcon
+            :name="fileIconMapping[doc.file.split('.').slice(-1)[0] as keyof typeof fileIconMapping]"
+            class="mr-2 size-5 shrink-0"
+          />
+          <NuxtLink
+            :to="`${config.public.mediaUrl}/${doc.file}`"
+            class="underline hover:text-gray-700 dark:hover:text-gray-200"
+            external
+            target="_blank"
+          >
             {{ doc.title }}
           </NuxtLink>
         </div>
@@ -43,13 +56,23 @@ const fileIconMapping = {
             class="flex items-center justify-center"
             :popper="{ placement: 'left' }"
           >
-            <UIcon name="i-mdi-information" class="size-5" />
+            <UIcon
+              name="i-mdi-information"
+              class="size-5"
+            />
             <template #panel>
               <div class="px-4 py-1 text-xs font-bold">
                 Размещено в категориях:
               </div>
-              <div v-for="cat in doc.categories" :key="cat.id" class="px-4 py-1 text-xs">
-                <NuxtLink :to="`/catalog/${cat.slug}`" class="underline hover:text-gray-700 dark:hover:text-gray-200">
+              <div
+                v-for="cat in doc.categories"
+                :key="cat.id"
+                class="px-4 py-1 text-xs"
+              >
+                <NuxtLink
+                  :to="`/catalog/${cat.slug}`"
+                  class="underline hover:text-gray-700 dark:hover:text-gray-200"
+                >
                   {{ cat.name }}
                 </NuxtLink>
               </div>
