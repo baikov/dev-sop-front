2a3aaa90ab8e58fe41d17f84a7c2ae8bff748d6e
Alexey Baikov <baikov52@gmail.com>
2024-11-09 23:54:03 +0300

    ~ fix document size

--- utils/base.ts
+++ utils/base.ts
@@ -1,4 +1,5 @@
 export const fileSize = (size: number) => {
+  if (size === 0) return '0 B'
   const i = Math.floor(Math.log(size) / Math.log(1024))
   return (size / Math.pow(1024, i)).toFixed(2) + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i]
 }
