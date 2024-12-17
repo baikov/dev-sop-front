b6078d1a3d22325fb549a76da9ec347635981abf
Alexey Baikov <baikov52@gmail.com>
2024-12-18 00:04:33 +0300

    fix: update footer version from v0.15.2 to v0.15.3 and improve trailing slash redirection logic

--- server/middleware/redirect-trailing-slash.ts
+++ server/middleware/redirect-trailing-slash.ts
@@ -3,7 +3,7 @@ import { parseURL } from 'ufo'
 export default defineEventHandler(async (event) => {
   const { pathname } = parseURL(event.node.req.url)
 
-  if (pathname.endsWith('/')) {
+  if (pathname.endsWith('/') && pathname !== '/') {
     event.node.res.writeHead(301, { Location: pathname.slice(0, -1) })
     event.node.res.end()
   }
