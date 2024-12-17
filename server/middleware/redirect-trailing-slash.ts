4d8ac2b8f966b6456864b799b6b8197b7592f6be
Alexey Baikov <baikov52@gmail.com>
2024-12-17 22:47:19 +0300

    feat: add middleware to redirect trailing slashes in URLs SOP-72

---
+++ server/middleware/redirect-trailing-slash.ts
@@ -0,0 +1,10 @@
+import { parseURL } from 'ufo'
+
+export default defineEventHandler(async (event) => {
+  const { pathname } = parseURL(event.node.req.url)
+
+  if (pathname.endsWith('/')) {
+    event.node.res.writeHead(301, { Location: pathname.slice(0, -1) })
+    event.node.res.end()
+  }
+})
