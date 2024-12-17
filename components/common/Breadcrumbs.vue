1f2311eed2571a4e7f86563f85d3666fa458a356
Alexey Baikov <baikov52@gmail.com>
2024-12-17 22:15:41 +0300

    fix: remove last item link SOP-77

--- components/common/Breadcrumbs.vue
+++ components/common/Breadcrumbs.vue
@@ -37,25 +37,35 @@ useSchemaOrg([
             </NuxtLink>
           </div>
         </li>
-        <li
-          v-for="item in items"
-          :key="item.level"
-          class="flex whitespace-nowrap"
-        >
-          <div class="flex items-center">
-            <UIcon
-              name="i-mdi-chevron-right"
-              class="h-full w-6 shrink-0"
-            />
-            <NuxtLink
-              :to="item.href"
-              class="ml-4 text-sm font-medium hover:underline"
-              :aria-current="item.href ? 'page' : undefined"
-            >
-              {{ item.name }}
-            </NuxtLink>
-          </div>
-        </li>
+        <template v-if="items && items.length > 0">
+          <li
+            v-for="(item, index) in items"
+            :key="item.level"
+            class="flex whitespace-nowrap"
+          >
+            <div class="flex items-center">
+              <UIcon
+                name="i-mdi-chevron-right"
+                class="h-full w-6 shrink-0"
+              />
+              <NuxtLink
+                v-if="index !== items.length - 1"
+                :to="item.href"
+                class="ml-4 text-sm font-medium hover:underline"
+                :aria-current="item.href ? 'page' : undefined"
+              >
+                {{ item.name }}
+              </NuxtLink>
+              <span
+                v-else
+                class="ml-4 text-sm font-bold"
+                aria-current="page"
+              >
+                {{ item.name }}
+              </span>
+            </div>
+          </li>
+        </template>
       </ol>
     </nav>
   </UContainer>
